90c03466ba38f0b64fa5525bd942297ac2141c39
ajagravat3 <146569267+ajagravat3@users.noreply.github.com>
2023-10-06 23:55:04 +0530

    Add files via upload

---
+++ ChildToPerent.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+// import { useState } from "react"
+
+const ChildToPerent = (props) => {
+    
+    // const [date, setdate] = useState("kgjg,ucoivk jgcxzvcbo") 
+
+    return (
+        <>
+            {/* <h2>child to parent data : {props.name}</h2>
+            <button onClick={() => props.data(date)}>
+                Click
+            </button> */}
+        </>
+    )
+}
+
+export default ChildToPerent
